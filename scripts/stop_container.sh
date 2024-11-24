775021952385c5d961da41cc07a03cf27faebadc
harsh80889 <harshaws7933@gmail.com>
2024-11-24 18:24:07 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,9 @@
 set -e
 
 # Stop the running container (if any)
+#forcefully deleting the running container here when we mannually commit/make changes
+#in the code then code deployment was succesfull and thev docker image was deployed
+#and when we integrate the code pipeline in code deploy so code pipeline again
+#tried to deploy the same docker image and the docker image already exits so it gave
+#error, so we write the below code to delete the existing docker image
 docker rm -f
