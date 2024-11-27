a0946ec95877605ca6688a89653b67ad2ad62c46
harsh80889 <harshaws7933@gmail.com>
2024-11-27 21:38:36 +0530

    add the files for code deploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
+# Pull the Docker image from Dockers Hub
 docker pull harshf5/simple-python-flask-app:latest
 
 # Run the Docker image as a container
